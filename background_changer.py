f871b58fb3857892633edfa187cb9fc25ac4bed2
Gavrilenko-Andrey <germannvins@gmail.com>
2024-06-29 08:38:43 +0700

    Добавил комментарии и изменил аннотации.

--- background_changer.py
+++ background_changer.py
@@ -1,29 +1,59 @@
+"""
+Файл с классами, реализующими выделение и замену фона изображения
+"""
 import time
-
 import numpy as np
 import cv2
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 
 
 class BackgroundSubtractor:
-    def fit(self, background_image: np.array):
+    """
+    Основной (абстрактный) класс для отделения фона от переднего плана
+    """
+    def fit(self, background_image: np.ndarray) -> None:
+        """
+        Метод, применяющийся для запоминания фона без изображений на переднем плане
+        :param background_image: Изображение фона
+        :return: None
+        """
         pass
 
-    def subtract_background(self, image: np.array):
+    def subtract_background(self, image: np.ndarray) -> None:
+        """
+        Метод, применяющийся для отделения фона от изображения
+        :param image: Изображение
+        :return: Изображение "без" фона
+        """
         pass
 
-    def change_background(self, image: np.array, chosen_background: np.array):
+    def change_background(self, image: np.array, chosen_background: np.array) -> None:
+        """
+        Метод, заменяющий фон изображения на переданный
+        :param image: Изображение
+        :param chosen_background: Новый фон
+        :return: Изображение с измененным фоном
+        """
         pass
 
 
 class MySubtractor(BackgroundSubtractor):
+    """
+    Класс, реализующий "собственную" реализацию определения фона изображения. На данный момент в испытаниях показал себя хуже всего
+    """
     def __init__(self):
         self.background_image = None
 
-    def fit(self, background_image: np.array):
+    def fit(self, background_image: np.ndarray) -> None:
         self.background_image = background_image
 
-    def subtract_background(self, image: np.array, threshold: int = 5):
+    def subtract_background(self, image: np.ndarray, threshold: int = 5) -> np.ndarray:
+        """
+        Метод, применяющийся для отделения фона от изображения
+        :param image: Изображение
+        :param threshold: Пороговое значение, при котором пиксель считается причастным к фону. Чем выше значение - тем больше пикселей будут отнесены к фону
+        :return:
+        """
         diff = cv2.absdiff(self.background_image, image)
         mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
         imask = mask > threshold
@@ -33,7 +63,7 @@ class MySubtractor(BackgroundSubtractor):
 
         return transformed_image
 
-    def change_background(self, image: np.array, chosen_background: np.array):
+    def change_background(self, image: np.ndarray, chosen_background: np.ndarray) -> np.ndarray:
         my_image = self.subtract_background(image)
         mask = cv2.cvtColor(my_image, cv2.COLOR_BGR2GRAY)
         imask = mask == 0
@@ -42,22 +72,25 @@ class MySubtractor(BackgroundSubtractor):
 
 
 class CvSubtractor(BackgroundSubtractor):
+    """
+    Класс, реализующий разделение фона и изображение на основе модели (KNN) из библиотеки opencv. На данный момент в испытаниях показал среднее качество
+    """
     def __init__(self):
         self.model = cv2.createBackgroundSubtractorKNN()
         self.background_image = None
 
-    def fit(self, background_image: np.array):
+    def fit(self, background_image: np.ndarray) -> None:
         self.background_image = background_image
         for _ in range(50):
             self.model.apply(background_image)
 
-    def subtract_background(self, image: np.array):
+    def subtract_background(self, image: np.ndarray) -> np.ndarray:
         transformed_image = self.model.apply(image)
         for _ in range(50):
             self.model.apply(self.background_image)
         return transformed_image
 
-    def change_background(self, image: np.array, chosen_background: np.array):
+    def change_background(self, image: np.ndarray, chosen_background: np.ndarray) -> np.ndarray:
         my_image = self.subtract_background(image)
         # mask = cv2.cvtColor(my_image, cv2.COLOR_BGR2GRAY)
         imask = my_image == 0
@@ -66,22 +99,46 @@ class CvSubtractor(BackgroundSubtractor):
 
 
 class CvzoneSubtractor(BackgroundSubtractor):
+    """
+    класс, реализующий разделение фона и изображения на основе модели из библиотеки cvzone. На данный момент в испытаниях показал наилучшую производительность.
+    """
     def __init__(self):
         self.model = SelfiSegmentation()
 
-    def fit(self, background_image: np.array):
+    def fit(self, background_image: np.ndarray = None) -> None:
+        """
+        Метод, применяющийся для запоминания фона без изображений на переднем плане. Для данного класса может не применяться, оставлен для совместимости
+        :param background_image: Изображение фона
+        :return: None
+        """
         pass
 
-    def subtract_background(self, image: np.array, threshold: float = 0.2):
+    def subtract_background(self, image: np.ndarray, threshold: float = 0.2) -> np.ndarray:
+        """
+        Метод, применяющийся для отделения фона от изображения
+        :param image: Изображение
+        :param threshold: Пороговое значение, при котором пиксель считается причастным к фону. Чем выше значение - тем больше пикселей будут отнесены к фону
+        :return:
+        """
         img_out = self.model.removeBG(image, (0, 0, 0), cutThreshold=threshold)
         return img_out
 
-    def change_background(self, image: np.array, chosen_background: np.array, threshold: float = 0.2):
+    def change_background(self, image: np.ndarray, chosen_background: np.ndarray, threshold: float = 0.2) -> np.ndarray:
+        """
+        Метод, заменяющий фон изображения на переданный
+        :param image: Изображение
+        :param chosen_background: Новый фон
+        :param threshold: Пороговое значение, при котором пиксель считается причастным к фону. Чем выше значение - тем больше пикселей будут отнесены к фону
+        :return: Изображение с измененным фоном
+        """
         img_out = self.model.removeBG(image, chosen_background, cutThreshold=threshold)
         return img_out
 
 
 if __name__ == "__main__":
+    """
+    Если запустить этот файл, а не импортировать, то будут проведен ряд тестов.
+    """
     background_picture = cv2.resize(cv2.imread("2024-06-17_16-06-49.jpg"), (1920, 1080))
     mans_picture = cv2.resize(cv2.imread("2024-06-17_16-07-15.jpg"), (1920, 1080))
     new_background_picture = cv2.resize(cv2.imread("New_background.jpg"), (1920, 1080))
--- image_effects.py
+++ image_effects.py
@@ -1,52 +1,108 @@
+"""
+Файл с функциями, позволяющими применят эффекты к изображениям
+"""
 import time
 import numpy as np
 import cv2
 
 
-def negative(image: np.array):
+def negative(image: np.ndarray) -> np.ndarray:
+    """
+    Функция, возвращающая негатив заданного изображения
+    :param image: Изображение
+    :return: Негатив изображения, совпадающий по размерности с image
+    """
     return 255 - image
 
 
-def to_black_white(image: np.array):
+def to_black_white(image: np.ndarray) -> np.ndarray:
+    """
+    Функция, преобразующая цветное изображение в черно-белое
+    :param image: Изображение - массив формы (x, y, 3)
+    :return: Черно-белое изображение, полученное из image, размерности (x, y, 1)
+    """
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-def change_brightness_and_contrast(image: np.array, contrast: float = 1, brightness: int = 0):
+def change_brightness_and_contrast(image: np.ndarray, contrast: float = 1, brightness: int = 0) -> np.ndarray:
+    """
+    Функция, изменяющая яркость и контраст изображения
+    :param image: Изображение, которое нужно изменить
+    :param contrast: Величина контраста
+    :param brightness: Величина яркости
+    :return: Измененное изображение, рассчитывающееся по формуле: image * conntrast + brightness
+    """
     return cv2.addWeighted(image, contrast, np.zeros(image.shape, image.dtype), 0, brightness)
 
 
-def sharpen_image(image: np.array, kernel: np.array = None):
+def sharpen_image(image: np.ndarray, kernel: np.ndarray = None) -> np.ndarray:
+    """
+    Функция, увеличивающая реззкость изображения. Применяет свертку.
+    :param image: Изображение
+    :param kernel: Ядро свертки. В случае None будет использовано ядро, увеличивающее резкость.
+    :return: Изображение, после применения к нему операции свертки
+    """
     if not kernel:
         kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     return cv2.filter2D(image, -1, kernel)
 
 
-def sharpen_laplacian(image: np.array):
+def sharpen_laplacian(image: np.ndarray) -> np.ndarray:
+    """
+    Функция, увеличивающая резкость с помощью "Лапласиана"
+    :param image: Изображение
+    :return: Изоббражение с увеличенной резкостью
+    """
     return cv2.Laplacian(image, cv2.CV_64F)
 
 
-def blur_image(image: np.array, blur: int = 1):
+def blur_image(image: np.ndarray, blur: int = 1) -> np.ndarray:
+    """
+    Функция, размывающая изображение
+    :param image: Изображение
+    :param blur: Коэффициент размытия, ччем больше 1, тем больше размытие
+    :return: Изображение после применения размытия
+    """
     return cv2.medianBlur(image, blur)
 
 
-def remove_noise_gaussian(image: np.array, size_of_kernel: tuple = None, kernel_shift: int = 0):
+def remove_noise_gaussian(image: np.array, size_of_kernel: tuple = None, kernel_shift: int = 0) -> np.ndarray:
+    """
+    Функция, убирающая шум из изображения. Применяет свертку
+    :param image: Изображение
+    :param size_of_kernel: Размер ядра свертки
+    :param kernel_shift: Смещение ядра свертки
+    :return: Преобразованное изображение
+    """
     if size_of_kernel is None:
         size_of_kernel = (5, 5)
     return cv2.GaussianBlur(image, size_of_kernel, kernel_shift)
 
 
-def change_colors(image: np.array):
+def change_colors(image: np.ndarray):
     pass
 
 
-def dilate(image: np.array, kernel: np.array = None):
+def dilate(image: np.ndarray, kernel: np.ndarray = None) -> np.ndarray:
+    """
+    Функция, применяющая эффект "расширения к изображению. Все линии становятся шире. Использует свертку"
+    :param image: Изображение
+    :param kernel: Ядро ссвертки. Если None - будет применено ядро для расширения
+    :return: Преобразованное изображение
+    """
     if not kernel:
         kernel = np.ones((3, 3))
     dilate_img = cv2.dilate(image, kernel=kernel, iterations=1)
     return dilate_img
 
 
-def erode(image: np.array, kernel: np.array = None):
+def erode(image: np.ndarray, kernel: np.ndarray = None) -> np.ndarray:
+    """
+    Функция, применяющая эффект "сжатия" к изображению. Все линии становятся уже. Использует свертку
+    :param image: Изображение
+    :param kernel: Ядро свертки. Если None - будет применено ядро для сжатия
+    :return: Преобразованное изображение
+    """
     if not kernel:
         kernel = np.ones((3, 3))
     erode_img = cv2.erode(image, kernel=kernel, iterations=1)
@@ -54,6 +110,9 @@ def erode(image: np.array, kernel: np.array = None):
 
 
 if __name__ == "__main__":
+    """
+    Если запустить этот файл, а не импортировать, то будут проведен ряд тестов.
+    """
     mans_picture = cv2.resize(cv2.imread("2024-06-17_16-07-15.jpg"), (1920, 1080))
 
     cv2.imshow("Testing window", negative(mans_picture))
