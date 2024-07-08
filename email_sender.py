66fc3800c5c8ff64d535d0420b1007977a4fc598
Gavrilenko-Andrey <germannvins@gmail.com>
2024-07-08 14:02:29 +0700

    Добавил функция отправки email и комментарии.

--- email_sender.py
+++ email_sender.py
@@ -8,6 +8,16 @@ def send_email(filename: str, receiver_email: str = "an.s.gavrilenko@gmail.com",
                subject: str = "An email with attachment from Python4",
                body: str = "This is an email with attachment sent from Python",
                sender_email: str = "gmandrtest@gmail.com", password: str = "wsjpkirfwqvgbucx"):
+    """
+    Функция для отправки email с вложением с использованием сервера gmail. Receiver email и password уже настроены.
+    :param filename: Имя файла - изображения, который нужно отправить
+    :param receiver_email: Email получателя
+    :param subject: Тема письма
+    :param body: Тело письма (текст)
+    :param sender_email: Email отправителя
+    :param password: Пароль для доступа к поччте отправителя
+    :return:
+    """
     smtp_server = "smtp.gmail.com"
 
     msg = EmailMessage()
@@ -34,3 +44,7 @@ def send_email(filename: str, receiver_email: str = "an.s.gavrilenko@gmail.com",
             print(e)
         finally:
             server.quit()
+
+
+if __name__ == "__main__":
+    send_email("2024-06-17_16-06-49.jpg")
